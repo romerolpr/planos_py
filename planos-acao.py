e37339bd3d5226ef7bbe8c5bd7fa9524bf770c26
Lucas Romero <lucas.romero@doutoresdaweb.com.br>
2020-09-15 20:32:31 -0300

    update files

--- planos-acao.py
+++ planos-acao.py
@@ -3,20 +3,19 @@ from tqdm.auto import tqdm
 from requests_html import HTMLSession
 from colorama import Fore, Style
 
-session = HTMLSession()
-
-# variáveis do projeto
-# projeto = ''
-
 print(Fore.YELLOW)
 projeto = str(input('Por favor, especifique o nome do projeto:\n'))
 print(Style.RESET_ALL)
 
-htdocs = f'C://xampp/htdocs/{projeto}/' # alterar para htdocs proprio
+# alterar para htdocs proprio
+htdocs = f'C://xampp/htdocs/{projeto}/'
+
+# código a ser inserido
+code = 'include(\'btn-modal.php\');'
 
 # inserir os arquivos para serem editados (sem .php)
 f = []
-g = 0
+g = []
 
 def get_mpis(URL):
     rm = session.get(URL + 'mapa-site')
@@ -26,6 +25,7 @@ def get_mpis(URL):
         f.append(linkMPI.attrs['href'].split('/')[-1])
 
 Error = { 'Não foi possível ler o(s) arquivo(s)':[],'Não foi possível criar o arquivo':[],'Não foi possível realizar o ajustes no(s) arquivo(s)':[],'Não foi possível recuperar o título da página':[], 'Não foi possível inserir após o H2': [], 'Falha na execução.': [], }
+Log = { f'Não foi possível inserir "{code}" no arquivo': [],}
 
 # le arquivo e recupera valores
 def file_read(f):
@@ -43,13 +43,13 @@ def file_read(f):
 		Error['Não foi possível ler o(s) arquivo(s)'].append(f'=> {f}.php')
 
 # montar url do temporario
-def urlReplace(x, y):
-	x = x.split('//')
-	r = x[1].split('/')
-	if not y:
-		return 'http://mpitemporario.com.br/projetos/' + r[2] + '/'
+def urlReplace(x, file):
+	x = x.split('//')[1]
+	project = x.split('/')[2] if 'git' not in htdocs.lower() else x.split('/')[3] 
+	if not file:
+		return 'http://mpitemporario.com.br/projetos/' + project + '/'
 	else:
-		return 'http://mpitemporario.com.br/projetos/' + r[2] + '/' + y
+		return 'http://mpitemporario.com.br/projetos/' + project + '/' + file
 	
 # variáveis para mascara
 elements = []
@@ -92,14 +92,14 @@ def create(body, file):
 	    # faz a criacao dos arquivos
 		with open(f'./projetos/{arquivo}' + '.php', 'w', encoding='utf-8') as f:
 
-			body = body.replace('<!-- include(\'inc/btn-modal.php\'); -->', "<? include('inc/btn-modal.php'); ?>")
+			body = body.replace(f'<!-- {code} -->', f'<? {code} ?>')
 			#cria
 			f.write(body)
 			f.write('</html>')
 	except: 
 	    Error['Não foi possível criar o arquivo'].append(f'=> {file}.php')
 
-# faz o ajuste nos strongs do projeto
+# insere o codigo
 def add_content(t, html, a):
 
 	# armazenando elementos
@@ -115,15 +115,15 @@ def add_content(t, html, a):
 		for article in soup.select('article'):
 
 			new_tag = soup.new_tag("div")
-			new_tag.append('<!-- include(\'inc/btn-modal.php\'); -->')
+			new_tag.append(f'<!-- {code} -->')
 
 			h2 = article.find('h2')
 
-			# verifica se o paragrafo tem strong
+			# verifica se o article tem h2
 			if h2:
 				h2.insert_after(new_tag)
 			else:
-				Error['Não foi possível inserir após o H2'].append(f'\n=> {a}')	
+				Log[f'Não foi possível inserir "{code}" no arquivo'].append(f'\n=> {a}')	
 
 
 		# retorna novo código
@@ -141,40 +141,50 @@ def add_content(t, html, a):
 print(Fore.YELLOW)
 print('Iniciando Plano De Ação... Aguarde\n', Style.RESET_ALL)
 
+session = HTMLSession()
+
 if projeto:
 	try:
-		get_mpis(urlReplace(htdocs, False))
-		# print(f)
+		
+		try:
 
-		for a in tqdm(f):
+			get_mpis(urlReplace(htdocs, False))
 
-			r = session.get(urlReplace(htdocs, a))
-			# constroi o arquivo
-			html = file_read(htdocs + a.strip())
-			# retorna o title da pagina
-			try:
-				t = r.html.find('head title')
-				for v in t:
-					title = v.text.split('-')[0]
-			except:
-				Error['Não foi possível recuperar o título da página'].append(f'=> {urlReplace(htdocs, a)}')
-			else:
+		except:
+
+			Error['Não foi possível recuperar as palavras chaves do projeto'].append(f'=> {urlReplace(htdocs, a)}')
+
+		else:
+
+			for a in tqdm(f):
+
+				r = session.get(urlReplace(htdocs, a))
+				# constroi o arquivo
+				html = file_read(htdocs + a.strip())
+				# retorna o title da pagina
 				try:
-					
-					# Após receber todos os valores com sucesso, realiza os ajustes e retira a máscara do código
-					body = add_content(title, html, a)
-
-					if body != False:
-						create(body, a)
-						# print(body)
-						g += 1
-					else:
-						Error['Falha na execução.'].append(f'=> {a}.php')
-
+					t = r.html.find('head title')
+					for v in t:
+						title = v.text.split('-')[0]
 				except:
-					Error['Não foi possível realizar o ajustes no(s) arquivo(s)'].append(f'=> {a}.php')
+					Error['Não foi possível recuperar o título da página'].append(f'=> {urlReplace(htdocs, a)}')
+				else:
+					try:
+						
+						# Após receber todos os valores com sucesso, realiza os ajustes e retira a máscara do código
+						body = add_content(title, html, a)
 
-				del elements[:]
+						if body != False:
+							# create(body, a)
+							# print(body)
+							g.append(a)
+						else:
+							Error['Falha na execução.'].append(f'=> {a}.php')
+
+					except:
+						Error['Não foi possível realizar o ajustes no(s) arquivo(s)'].append(f'=> {a}.php')
+
+					del elements[:]
 			
 	except:
 		print(Fore.RED)
@@ -189,20 +199,36 @@ if projeto:
 # 	File.seek(-1)
 # 	File.write('\n.popup{position:fixed;z-index:999;top:50px;left:0;right:0;margin:10px auto;width:400px;max-width:90%;background:#fff;border-radius:3px;box-shadow:0 2px 2px rgba(0,0,0,.5);display:flex;flex-wrap:wrap;justify-content:center;padding:20px;border:1px solid #f5f5f5}.popup p{font-size:24px;text-align:center;width:100%;margin:10px 0}.popup a{width:calc(50% - 20px);background:#f80000;margin:10px;text-align:center;padding:10px;color:#fff}#form-especialista{position:relative;float:left;width:100%;height:100%;box-sizing:border-box;font-family:"Helvetica Neue",Helvetica,Arial,sans-serif;padding:20px}#form-cta-form-rotulos-para-bebidas{background:#e2e2e2}.p-form{box-sizing:border-box;padding:0 40px;padding-top:20px}.p-form h2{font-size:32px;line-height:32px;color:#333233;display:block;font-weight:700;text-align:center;margin-bottom:10px;padding:0}.p-form h3{color:#b7b7b7;font-size:22px;line-height:28px;font-weight:400}.p-form h2,.p-form h3{text-align:center}#form-especialista .p-form{box-sizing:border-box;padding:20px}#form-especialista label{float:left;width:100%;color:#969696;font-size:16px;margin:0}#form-especialista input,#form-especialista select,#form-especialista textarea{outline:0;width:100%;box-sizing:border-box;padding:8px 5px;background:#fff;font-size:16px;font-weight:400;border:none;border:1px solid #c8c8c8;margin:10px 0;font-weight:700;border-radius:3px}#form-especialista .radio{float:left;clear:both;width:100%;margin:0}#form-especialista .radio input{display:inline-block;background-color:#fff;vertical-align:middle;font-size:18px;line-height:20px;width:auto!important;height:35px;margin:0;margin-right:5px;padding:5px}#form-especialista input[name=autorizo_contato_por_telefone]{width:auto!important;margin-right:5px}#form-especialista .notice{color:#969696}#form-especialista #cf_submit-cta-form-rotulos-para-bebidas{cursor:pointer;height:auto;text-align:center;text-decoration:none;font-weight:700;font-size:20px;word-break:break-all;line-height:1.2em;white-space:normal;vertical-align:middle;margin:15px auto 24px auto;padding:15px 20px 17px 20px;-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px;background:#f80000;border:1px solid #d60505;color:#fff}#form-especialista #cf_submit-cta-form-rotulos-para-bebidas:hover{filter:brightness(.9)}')
 
-print(Fore.RED)
 # Exibe log na tela
+print(Fore.RED)
 for errosItens in Error.keys():
-
     if len(Error[errosItens]) > 0:
-        print(errosItens+'\n')
+
+        print(errosItens)
+
         for errosValores in Error[errosItens]:
             print(errosValores)
-        print('\n')
-    else:
-    	print(Fore.GREEN)
-    	print('Ajustes realizados com sucesso!')	
-    	break
 
-print(Style.RESET_ALL)
+        msg = 'Falha ao tentar executar 1 ou mais funções.'
+    else:
+    	msg = 'Foram realizados ({}) planos de ação no projeto.'.format(len(g))
+
+print(Fore.YELLOW)
+for logItens in Log.keys():
+	if len(Log[logItens]) > 0:
+	    print('!!! AVISO !!!\n')
+	    print(logItens)
+	    for logValores in Log[logItens]:
+	        print(logValores)
+
+if len(g) > 0:
+	print(Fore.GREEN)
+else:
+	print(Fore.RED)
+
+print('\n')
+print(msg, Style.RESET_ALL)	
+
+print(urlReplace(htdocs, False))
 
 input('\nFinalizado. Aperte "ENTER" para encerrar o programa.')
