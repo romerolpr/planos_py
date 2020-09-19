94e0a79d8a594f4a68c089e368cae33be5ca86e3
Lucas Romero <lucas.romero@doutoresdaweb.com.br>
2020-09-19 17:53:47 -0300

    interface 1

--- planos-acao.py
+++ planos-acao.py
@@ -1,170 +1,207 @@
-# Leia README.md para utilizar a aplicação corretamente.
+# v1.0 (Beta)
 
+import os
 from bs4 import BeautifulSoup, Comment
 from tqdm.auto import tqdm
 from requests_html import HTMLSession
-from colorama import Fore, Style
+from colorama import Fore, Style, init
+import sys
 
-print(Fore.YELLOW)
-projeto = str(input('Por favor, especifique o nome do projeto: '))
-print(Style.RESET_ALL)
+# Variáveis Pessoais (alterar)
+htdocs 	= 'C://xampp/htdocs/'
+code 	= 'include(\'inc/btn-modal.php\');' # Código a ser inserido nos aquivos
+vAll 	= True # Quando True resgata todas as MPI'S automaticamente
+f 		= []
 
-# alterar para htdocs proprio
-htdocs = f'C://xampp/htdocs/{projeto}/'
+# Sistema
+Start 	= False
 
-# código a ser inserido
-code = 'include(\'inc/btn-modal.php\');'
+init(autoreset=True)
 
-# "True" aplica o plano em todas as palavras chaves do projeto. 
-# "False", utiliza a lista "f" para realizar os planos
-vAll = True
-f = []
-g = []
+while True:
 
-def get_mpis(URL):
-	rm = session.get(URL + 'mapa-site')
-	subMenuInfo = rm.html.find('.sitemap ul.sub-menu-info li a')
-	for linkMPI in subMenuInfo:
-		f.append(linkMPI.attrs['href'].split('/')[-1])
+	terminal = ''
 
-Error = { 'Não foi possível ler o(s) arquivo(s)':[],'Não foi possível criar o arquivo':[],'Não foi possível realizar o ajustes no(s) arquivo(s)':[],'Não foi possível recuperar o título da página':[], 'Não foi possível inserir após o H2': [], 'Falha na execução.': [], 'Não foi possível montar a sessão do projeto': [], }
-Log = { f'Não foi possível inserir "{code}" no arquivo': [],}
+	# comandos do sistema
+	def comands(console):
+		if '-' == console:
+			print(Fore.WHITE + f'"{console}" Comando não reconhecido')
+			print(Fore.WHITE + '-help para abrir o guia de uso.')
+		if '-help' in console:
+			print(Fore.WHITE + 'planos_py, ' + Fore.GREEN + 'versão 1.0' + Fore.YELLOW + ' (beta)', Style.DIM)
+			print(Fore.WHITE + '\nComandos nativos do sistema')
+			print(Fore.WHITE + ' -a     ─────── Iniciar programa.')
+			print(Fore.WHITE + ' -code  ─────── Alterar código de inclusão.')
+			print(Fore.WHITE + ' -help  ─────── Exibe comandos do sistema.')
+			print(Fore.WHITE + ' -clear ─────── Limpa a tela sempre que precisar.')
+			print(Fore.WHITE + ' -exit  ─────── Encerra o programa.')
+		if ' -clear' in console or '-clear' == console:
+			clear = lambda: os.system('cls')
+			clear()
+		if '-exit' == console:
+			sys.exit()
 
-# le arquivo e recupera valores
-def file_read(f):
-	content = []
-	import os.path
-	try:
-		with open(f + '.php', 'r', encoding='utf-8') as file:
-			lines = file.readlines()
-			for elem in lines:
-				content.append(elem)
-				# string converter
-			return ''.join(map(str, content))
-	except IOError:
-		return False	
-
-# montar url do temporario
-def urlReplace(x, file):
-	x = x.split('//')[1]
-	project = x.split('/')[2] if 'git' not in htdocs.lower() else x.split('/')[3] 
-	if not file:
-		return 'http://www.' + project + '/'
-	else:
-		return 'http://www.' + project + '/' + file
-	
-# variáveis para mascara
-elements = []
-msk = '!!!PHP!!!'
-
-# funções pra fazer a remoção
-def remove(d):
-    elements.append(d.group())
-    return msk
-def add(e):
-    return elements.pop(0)
-
-# funcao para aplicar/ retirar mascara no codigo
-def mask(c, i):
-	import re
-	try:
-
-		# aplica a mascara
-		m = re.sub(r"<\?.*\?>", remove, c)
-		soup = BeautifulSoup(m, "html.parser")
-		if i == True:
-			mask = re.sub(msk, remove, soup.prettify())
+	# projeto
+	while ' -a' not in terminal:
+		print(Fore.YELLOW + '\nEspecifique o nome do projeto' + Fore.CYAN + ' (-a para iniciar)')
+		terminal = str(input('$ '))
+		# definindo funções do console
+		if terminal not in ' -a':
+			comands(terminal)
 		else:
-			mask = re.sub(msk, add, soup.prettify())
-	except:
-		mask = False
+			Start = True if terminal else False
 
-	return mask
+	# Variáveis interativas
+	projeto 	= terminal.split(' -a')[0].strip()
+	workplace 	= htdocs + projeto + '/'
+	g 			= []
 
-# cria o arquivo
-def create(body, file):
-	from pathlib import Path
-	arquivo = projeto + '/' + file
-	# realiza a criacao dos arquivos
-	try:
+	# Programa
 
-		# faz a criacao da pasta
-		Path(f'./projetos/{projeto}').mkdir(parents=True, exist_ok=True)
+	def get_mpis(URL):
+		rm = session.get(URL + 'mapa-site')
+		subMenu = rm.html.find('.sitemap ul.sub-menu-info')
+		subMenuInfo = rm.html.find('.sitemap ul.sub-menu-info li a') if subMenu else rm.html.find('.sitemap ul.sub-menu li a')
+		for linkMPI in subMenuInfo:
+			f.append(linkMPI.attrs['href'].split('/')[-1])
 
-	    # faz a criacao dos arquivos
-		with open(f'./projetos/{arquivo}' + '.php', 'w', encoding='utf-8') as f:
+	Error = { 'Não foi possível ler o(s) arquivo(s)':[],'Não foi possível criar o arquivo':[],'Não foi possível realizar o ajustes no(s) arquivo(s)':[],'Não foi possível recuperar o título da página':[], 'Não foi possível inserir após o H2': [], 'Falha na execução.': [], 'Não foi possível montar a sessão do projeto': [], }
+	Log = { f'Não foi possível inserir "{code}" no arquivo': [],}
 
-			body = body.replace(f'<!-- {code} -->', f'<? {code} ?>')
-			#cria
-			f.write(body)
-			f.write('</html>')
-	except: 
-	    Error['Não foi possível criar o arquivo'].append(f'=> {file}')
+	# le arquivo e recupera valores
+	def file_read(f):
+		content = []
+		import os.path
+		try:
+			with open(f + '.php', 'r', encoding='utf-8') as file:
+				lines = file.readlines()
+				for elem in lines:
+					content.append(elem)
+					# string converter
+				return ''.join(map(str, content))
+		except IOError:
+			return False	
 
-# insere o codigo
-def add_content(t, html, a):
+	# montar url do temporario
+	def url_replace(x, file):
+		x = x.split('//')[1]
+		# project = x.split('/')[2] if 'git' not in workplace.lower() else x.split('/')[3] 
+		if not file:
+			return 'http://mpitemporario.com.br/projetos/' + projeto + '/'
+			print(projeto)
+		else:
+			return 'http://mpitemporario.com.br/projetos/' + projeto + '/' + file
+		
+	# variáveis para mascara
+	elements = []
+	msk = '!!!PHP!!!'
 
-	# armazenando elementos
-	content = []
-	try:
+	# funções pra fazer a remoção
+	def remove(d):
+	    elements.append(d.group())
+	    return msk
+	def add(e):
+	    return elements.pop(0)
 
-		# criando o soup em html
-		soup = BeautifulSoup(mask(html, True), "html.parser")
+	# funcao para aplicar/ retirar mascara no codigo
+	def mask(c, i):
+		import re
+		try:
 
-		# tenta rodar os ajustes
-
-		for article in soup.select('article'):
-
-			new_tag = soup.new_tag("div")
-			new_tag.append(f'<!-- {code} -->')
-
-			h2 = article.find('h2')
-
-			# verifica se o article tem h2
-			if h2:
-				h2.insert_after(new_tag)
+			# aplica a mascara
+			m = re.sub(r"<\?.*\?>", remove, c)
+			soup = BeautifulSoup(m, "html.parser")
+			if i == True:
+				mask = re.sub(msk, remove, soup.prettify())
 			else:
-				Log[f'Não foi possível inserir "{code}" no arquivo'].append(f'\n=> {a}')	
+				mask = re.sub(msk, add, soup.prettify())
+		except:
+			mask = False
+
+		return mask
+
+	# cria o arquivo
+	def create(body, file):
+		from pathlib import Path
+		arquivo = projeto + '/' + file
+		# realiza a criacao dos arquivos
+		try:
+
+			# faz a criacao da pasta
+			Path(f'./projetos/{projeto}').mkdir(parents=True, exist_ok=True)
+
+		    # faz a criacao dos arquivos
+			with open(f'./projetos/{arquivo}' + '.php', 'w', encoding='utf-8') as f:
+
+				body = body.replace(f'<!-- {code} -->', f'<? {code} ?>')
+				#cria
+				f.write(body)
+				f.write('</html>')
+		except: 
+		    Error['Não foi possível criar o arquivo'].append(f'=> {file}')
+
+	# insere o codigo
+	def add_content(t, html, a):
+
+		# armazenando elementos
+		content = []
+		try:
+
+			# criando o soup em html
+			soup = BeautifulSoup(mask(html, True), "html.parser")
+
+			# tenta rodar os ajustes
+
+			for article in soup.select('article'):
+
+				new_tag = soup.new_tag("div")
+				new_tag.append(f'<!-- {code} -->')
+
+				h2 = article.find('h2')
+
+				# verifica se o article tem h2
+				if h2:
+					h2.insert_after(new_tag)
+				else:
+					Log[f'Não foi possível inserir "{code}" no arquivo'].append(f'\n=> {a}')	
 
 
-		# retorna novo código
-		for elem in soup.prettify(formatter=None):
-			content.append(elem)
-		value = ''.join(map(str, content))
+			# retorna novo código
+			for elem in soup.prettify(formatter=None):
+				content.append(elem)
+			value = ''.join(map(str, content))
 
-		return mask(value, False)
+			return mask(value, False)
 
-	except:
-		return False
+		except:
+			return False
+
+	# Inicia função principal para executar as correções
+	print(Fore.YELLOW + '\nIniciando plano de ação... Aguarde\n')
+
+	session = HTMLSession()
 
 
-# Inicia função principal para executar as correções
-print(Fore.YELLOW)
-print('Iniciando plano de ação... Aguarde\n', Style.RESET_ALL)
-
-session = HTMLSession()
-
-if projeto:
 	try:
 		
 		try:
 
 			if vAll:
-				get_mpis(urlReplace(htdocs, False))
+				get_mpis(url_replace(workplace, False))
 
 		except:
 
-			Error['Não foi possível recuperar as palavras chaves do projeto'].append(f'=> {urlReplace(htdocs, a)}')
+			Error['Não foi possível recuperar as palavras chaves do projeto'].append(f'=> {url_replace(workplace, a)}')
 
 		else:
 
 			for a in tqdm(f):
 
 				# monta sessão
-				r = session.get(urlReplace(htdocs, a))
+				r = session.get(url_replace(workplace, a))
 
 				# lê e monta o arquivo
-				html = file_read(htdocs + a.strip())
+				html = file_read(workplace + a.strip())
 				if not html:
 					# exibe erro se nao conseguir ler o arquivo
 					Error['Não foi possível ler o(s) arquivo(s)'].append(f'=> {f}')
@@ -175,7 +212,7 @@ if projeto:
 					for v in t:
 						title = v.text.split('-')[0]
 				except:
-					Error['Não foi possível recuperar o título da página'].append(f'=> {urlReplace(htdocs, a)}')
+					Error['Não foi possível recuperar o título da página'].append(f'=> {url_replace(workplace, a)}')
 				else:
 					try:
 						
@@ -195,36 +232,37 @@ if projeto:
 					del elements[:]
 			
 	except:
-		print(Fore.RED)
-		print('Não foi possível iniciar a função.', Style.RESET_ALL)
+		print(Fore.RED + 'Não foi possível iniciar a função.')
 
-# Exibe log na tela
-print(Fore.RED)
-for errosItens in Error.keys():
-    if len(Error[errosItens]) > 0:
+	# Exibe log na tela
+	for errosItens in Error.keys():
+	    if len(Error[errosItens]) > 0:
 
-        print(errosItens)
+	        print(Fore.RED + errosItens)
 
-        for errosValores in Error[errosItens]:
-            print(errosValores)
+	        for errosValores in Error[errosItens]:
+	            print(Fore.RED + errosValores)
 
-        msg = 'Falha ao tentar executar 1 ou mais funções.'
-    else:
-    	msg = 'Foram realizados ({}) planos de ação no projeto\n=> {}'.format(len(g), urlReplace(htdocs, False))
+	        msg = 'Falha ao tentar executar 1 ou mais funções.'
+	    else:
+	    	msg = 'Foram realizados ({}) planos de ação no projeto\n=> {}'.format(len(g), url_replace(workplace, False))
 
-print(Fore.YELLOW)
-for logItens in Log.keys():
-	if len(Log[logItens]) > 0:
-	    print('!!! AVISO !!!\n')
-	    print(logItens)
-	    for logValores in Log[logItens]:
-	        print(logValores)
+	for logItens in Log.keys():
+		if len(Log[logItens]) > 0:
+		    print('!!! AVISO !!!\n')
+		    print(Fore.YELLOW + logItens)
+		    for logValores in Log[logItens]:
+		        print(Fore.YELLOW + logValores)
 
-if len(g) > 0:
-	print(Fore.GREEN)
-else:
-	print(Fore.RED)
+	print('\n')
+	if len(g) > 0:
+		print(Fore.GREEN + msg)
+	else:
+		print(Fore.RED + msg)	
 
-print(msg, Style.RESET_ALL)	
 
-input('\nFinalizado. Aperte "ENTER" para encerrar o programa.')
+	# print(Fore.YELLOW + '\nProcesso Finalizado. Deseja reiniciar o programa? ' + Fore.CYAN + '(Y/ N)')
+	# Final = str(input('$ ')).upper()
+
+	Start = False
+	workplace = htdocs
